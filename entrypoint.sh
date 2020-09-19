cabb663a1165e31130d7ed4a468583788e74fc0f
Carl Egbert <egbertcarl@gmail.com>
2020-09-19 16:30:37 -0700

    change args setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,10 @@ mkdir -p "$SSH_PATH"
 echo "$SSH_PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
-RSYNC_OPTIONS="$1"
-RSYNC_SOURCE="$2"
-RSYNC_TARGET="$3"
+RSYNC_SOURCE="$1"
+RSYNC_TARGET="$2"
 
-echo "$RSYNC_OPTIONS $RSYNC_SOURCE $RSYNC_TARGET"
+echo "$RSYNC_SOURCE $RSYNC_TARGET"
 
 # Do deployment
 sh -c "rsync ${RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' ${GITHUB_WORKSPACE}${RSYNC_SOURCE} ${SSH_USERNAME}@${SSH_HOSTNAME}:${RSYNC_TARGET}"
