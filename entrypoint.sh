aa7b5fc80799f68b0811be22c6f31c522c0b14e6
Carl Egbert <egbertcarl@gmail.com>
2020-09-19 16:17:46 -0700

    add action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,11 @@ mkdir -p "$SSH_PATH"
 echo "$SSH_PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
+RSYNC_OPTIONS="$1"
+RSYNC_SOURCE="$2"
+RSYNC_TARGET="$3"
+
+echo "$RSYNC_OPTIONS $RSYNC_SOURCE $RSYNC_TARGET"
+
 # Do deployment
-sh -c "rsync ${INPUT_RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' ${GITHUB_WORKSPACE}${INPUT_RSYNC_SOURCE} ${SSH_USERNAME}@${SSH_HOSTNAME}:${INPUT_RSYNC_TARGET}"
+sh -c "rsync ${RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' ${GITHUB_WORKSPACE}${RSYNC_SOURCE} ${SSH_USERNAME}@${SSH_HOSTNAME}:${RSYNC_TARGET}"
